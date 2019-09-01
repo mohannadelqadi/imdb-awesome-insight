e8fe94889905da0817168b52217f00cf01e978a7
Mohannad Abd Almohdi <mohannad_a@administrators-MacBook-Pro-2.local>
2019-09-01 16:27:36 +0400

    init commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <h1>Redux</h1>
+            <p>
+                Redux is a post-positive adjective meaning "brought back, restored" (from Latin reducere, "to bring back")[1] used in literature, film and video game titles.
+            </p>
+            <p>
+                Works of literature using the word in the title include John Dryden's Astraea Redux (1662), "a poem on the happy restoration and return of His Sacred Majesty"; Anthony Trollope's Phineas Redux (1873), the sequel to Phineas Finn (1867); and John Updike's Rabbit Redux (1971), the second in his sequence of novels about the character Rabbit Angstrom.
+            </p>
+            <p>
+                Rabbit Redux led to a return in the popularity of the word redux and, in Rabbit at Rest (1990), Rabbit Angstrom notices "a story [...] in the Sarasota paper a week or so ago, headlined Circus Redux. He hates that word, you see it everywhere, and he doesn't know how to pronounce it. Like arbitrageur and perestroika."[2]
+            </p>
+            <p>
+                The term has been adopted by filmmakers to denote a new interpretation of an existing work by the restoration of previously removed material. This trend began with Apocalypse Now Redux, which Francis Ford Coppola released in 2001, re-editing and extending his original 1979 movie.[citation needed][original research?]
+            </p>
+            <p>
+                The term has also been used by music producers to describe what is more often referred to as a remix or remaster.
+            </p>
+        </div>
+    )
+}
